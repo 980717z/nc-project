a14a6e83662277a285e87903b45934bfdbc03afa
Jimmy <jimmylanbcn@gmail.com>
2020-02-28 17:40:22 -0500

    Initialize npm

---
+++ js-intro/index.js
@@ -0,0 +1,11 @@
+/**
+ * This file provides some incomplete functions for you
+ * to practice javascript syntax. Complete the required functions
+ * based on comments and doc string, then run `npm test` to 
+ * test your code.
+ */
+
+
+ function sumArrays(arr1=[], arr2=[]) {
+   
+ }
