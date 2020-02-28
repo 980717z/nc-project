527bc3ba887df093850af4a0dab5b0b155ee98c0
Jimmy <jimmylanbcn@gmail.com>
2020-02-28 18:12:54 -0500

    Complete test for sumArrays

--- js-intro/index.test.js
+++ js-intro/index.test.js
@@ -19,6 +19,10 @@ describe('sum two arrays by using sumArrays function', () => {
     expect(sumArrays([], [])).toEqual([]);
   });
 
+  test('sums [5] and [8]', () => {
+    expect(sumArrays([5], [8])).toEqual([13]);
+  })
+
   test('sums [1, 3, 5] and [2, 4, 6]', () => {
     let arr1 = [1, 3, 5];
     let arr2 = [2, 4, 6];
